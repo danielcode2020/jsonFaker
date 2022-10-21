3a3dbfb6bc6a8c388d094e1f5581d9f8ea70a6f4
danielcode2020 <danielmarandici2001@gmail.com>
2022-10-21 14:07:45 +0300

    Logging added

--- src/main/java/com/example/jsonfaker/configuration/AppConfiguration.java
+++ src/main/java/com/example/jsonfaker/configuration/AppConfiguration.java
@@ -1,6 +1,8 @@
 package com.example.jsonfaker.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -16,5 +18,10 @@ public class AppConfiguration {
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public Logger logger(){
+        return LoggerFactory.getLogger(this.getClass());
     }
 }
