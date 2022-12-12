0cdc5fa51c33726c352591e081ee4745b71837b0
daniel <danielmarandici2001@gmail.com>
2022-12-12 10:46:32 +0200

    Actuator added

--- src/main/java/com/example/jsonfaker/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/jsonfaker/security/config/WebSecurityConfig.java
@@ -11,9 +11,12 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
